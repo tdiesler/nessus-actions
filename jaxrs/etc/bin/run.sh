53906474d01da540cfb51c7d5eaaa2e7b50b5b1a
Thomas Diesler <tdiesler@redhat.com>
2020-10-10 11:47:50 +0200

    [resolves #32] Use a private network for inter-container communication

--- jaxrs/etc/bin/run.sh
+++ jaxrs/etc/bin/run.sh
@@ -1,10 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
 PRG="$0"
 
 # Get absolute path of the HOMEDIR
 HOMEDIR=`dirname $PRG`/..
 HOMEDIR=`cd $HOMEDIR; pwd`
+
+#######################################################
+#
+# KEYCLOAK CONFIG
+#
+if [[ -z "${KEYCLOAK_URL}" ]]; then
+    export KEYCLOAK_URL="http://keycloak:8080/auth"
+fi
+if [[ -z "${KEYCLOAK_USER}" ]]; then
+    export KEYCLOAK_USER="admin"
+fi
+if [[ -z "${KEYCLOAK_PASSWORD}" ]]; then
+    export KEYCLOAK_PASSWORD="admin"
+fi
+
+#######################################################
+#
+# TLS CONFIG
+#
+if [[ -z "${JAXRS_TLS_PORT}" ]]; then
+    export JAXRS_TLS_PORT=9443
+fi
+export JAXRS_TLS_CRT=/etc/x509/https/tls.crt
+export JAXRS_TLS_KEY=/etc/x509/https/tls.key
 
 JAVA_OPTS="-server"
 
